46fd6093675c5a9932e21a318620cdb6371d0d26
likesme <47811500+likesme@users.noreply.github.com>
2023-01-29 09:05:47 +0100

    Update register.php

--- register.php
+++ register.php
@@ -47,7 +47,7 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
 	$uniqid = uniqid();
 	$stmt->bind_param('ssss', $_POST['username'], $password, $_POST['email'], $uniqid);
 	$stmt->execute();
-	$from    = 'falinechine@gmail.com';
+	$from    = 'email@gmail.com';
 	$subject = 'Account Activation Required';
 	$headers = 'From: ' . $from . "\r\n" . 'Reply-To: ' . $from . "\r\n" . 'X-Mailer: PHP/' . phpversion() . "\r\n" . 'MIME-Version: 1.0' . "\r\n" . 'Content-Type: text/html; charset=UTF-8' . "\r\n";
 	// Update the activation variable below
